93a8e015b7c93a71cf2af5b194ec614dc1c5daac
53845714nF <seba98feustel@gmail.com>
2023-06-24 19:27:10 +0200

    app stuff

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,100 +1,60 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import App, {MAX_QUESTIONS} from "./App";
-//import * as React from "react";
+import * as quizData from "./QuizData";
+import {QuizQuestion} from "./AppTypes";
 
-const ActualQuizData = jest.requireActual('./QuizData')
+describe('Tests for App Component', () => {
 
-jest.mock("./QuizData", () => ({
-             __esModule: true,
-             quizData: [{
-                 id: 1,
-                 question: 'What is the capital of France?',
-                 answers: ['Paris', 'London', 'Berlin', 'Madrid'],
-                 correctAnswer: 'Paris',
-             },]
-}));
+    it('Render App Component', () => {
+        render(<App/>);
+        expect(screen.getByText("Typescript Quiz")).toBeInTheDocument();
+        expect(screen.getByRole('startButton')).toBeInTheDocument();
+    })
 
-describe('Test the App Component', () => {
-
-
-   it('Render App Component', () => {
-       render(<App/>);
-       expect(screen.getByText("Typescript Quiz")).toBeInTheDocument();
-       expect(screen.getByRole('startButton')).toBeInTheDocument();
-   })
-
-   it('Test the start button', () => {
-       render(<App/>);
-       const startButton = screen.getByRole('startButton');
-       fireEvent.click(startButton);
-       expect(screen.getByText("Score: 0")).toBeInTheDocument();
-   })
-
-   it('Test the next button', () => {
-       require("./QuizData").default = ActualQuizData.default;
-
-       render(<App/>);
-       const startButton = screen.getByRole('startButton');
-       fireEvent.click(startButton);
-       const nextButton = screen.getByRole('nextButton');
-       fireEvent.click(nextButton);
-       expect(screen.getByText("Question: 2 / 10")).toBeInTheDocument();
-   })
-
-   it('Test the nextQuestion function checks the end', () => {
-
-       jest.unmock("./QuizData")
-       jest.resetModules();
-
-       render(<App/>);
-       const startButton = screen.getByRole('startButton');
-       fireEvent.click(startButton);
-       const nextButton = screen.getByRole('nextButton');
-
-       for (let i = 0; i < MAX_QUESTIONS; i++) {
-           fireEvent.click(nextButton);
-       }
-       expect(screen.getByRole('startButton')).toBeInTheDocument();
-   })
-
-
-   it('checkAnswer updates score and user answers correctly', () => {
-         /*
-         const setScoreMock = jest.fn();
-         const setUserAnswersMock = jest.fn();
-         const useStateMock = jest.spyOn(React, 'useState');
-        */
-
-        render(<App />);
+    it('Test the start button', () => {
+        render(<App/>);
         const startButton = screen.getByRole('startButton');
         fireEvent.click(startButton);
+        expect(screen.getByText("Score: 0")).toBeInTheDocument();
+    })
 
-        /*
-        useStateMock.mockReturnValueOnce([0, jest.fn()]);
-        useStateMock.mockReturnValueOnce([[], setUserAnswersMock]);
-        useStateMock.mockReturnValueOnce([0, setScoreMock]);
-        useStateMock.mockReturnValueOnce([true, jest.fn()]);
-        useStateMock.mockReturnValueOnce([[{
-            question: 'What is the capital of France?',
-            answer: 'Paris',
-            correct: true,
-            correctAnswer: 'Paris',
-        }], jest.fn()]);
-        useStateMock.mockReturnValueOnce([0, jest.fn()]);
-        */
+    it('Test the next button', () => {
+        render(<App/>);
+        const startButton = screen.getByRole('startButton');
+        fireEvent.click(startButton);
+        const nextButton = screen.getByRole('nextButton');
+        fireEvent.click(nextButton);
+        expect(screen.getByText("Question: 2 / 10")).toBeInTheDocument();
+    })
 
+    it('Test the nextQuestion function checks the end', () => {
+        render(<App/>);
+        const startButton = screen.getByRole('startButton');
+        fireEvent.click(startButton);
+        const nextButton = screen.getByRole('nextButton');
+
+        for (let i = 0; i < MAX_QUESTIONS; i++) {
+            fireEvent.click(nextButton);
+        }
+        expect(screen.getByRole('startButton')).toBeInTheDocument();
+    })
+
+    it('Test checkAnswer updates score and user answers correctly', () => {
+        // Take me to 3 days to figure out how to mock the data
+        jest.spyOn(quizData, "getQuizData").mockReturnValue(
+            [{
+                id: 1,
+                question: "What is the capital of France?",
+                answers: ["Paris", "London", "Berlin", "Madrid"],
+                correctAnswer: "Paris",
+            } as QuizQuestion]
+        )
+
+        render(<App/>);
+        const startButton = screen.getByRole('startButton');
+        fireEvent.click(startButton);
         const answerButton = screen.getByText("Paris");
         fireEvent.click(answerButton);
         expect(screen.getByText("Score: 100")).toBeInTheDocument();
-
-        /*expect(setScoreMock).toHaveBeenCalledWith(100);
-        expect(setUserAnswersMock).toHaveBeenCalledWith([
-            {
-                question: expect.any(String), // Verify that the question is a string
-                answer: 'Paris',
-                correct: true,
-                correctAnswer: 'Paris',
-            },
-        ]);*/
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {QuestionCard} from "./components/QuestionCard";
-import  {quizData}  from "./QuizData";
+import {getQuizData} from "./QuizData";
 import {Button, Container, ThemeProvider, Typography, createTheme} from "@mui/material";
 import {Timer} from "./components/Timer";
 import {AnswerObject} from "./AppTypes";
@@ -9,118 +9,118 @@ import {WinningMessage} from "./components/WinningMessage.tsx";
 export const MAX_QUESTIONS = 10;
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#3178c6',
+    palette: {
+        primary: {
+            main: '#3178c6',
+        },
+        secondary: {
+            main: '#282c34',
+        },
     },
-    secondary: {
-      main: '#282c34',
-    },
-  },
 });
 
 export default function App() {
-  const [number, setNumber] = useState(0)
-  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
-  const [score, setScore] = useState(0);
-  const [gameOver, setGameOver] = useState(true);
-  const [randomQuizData, setRandomQuizData] = useState(quizData); // TODO:
-  const [timerValue, setTimerValue] = useState(0);
+    const [number, setNumber] = useState(0)
+    const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
+    const [randomQuizData, setRandomQuizData] = useState(getQuizData()); // TODO:
+    const [timerValue, setTimerValue] = useState(0);
 
-  function startTrivia() {
-    setNumber(0);
-    setUserAnswers([]);
-    setScore(0);
-    setGameOver(false);
-    setRandomQuizData(generateRandomList(quizData));
-    setTimerValue(0);
-  }
-
-  function generateRandomList<T>(elements: T[]): T[] {
-    const tempList = [...elements]; // Copy the initial List
-    const randomList: T[] = [];
-  
-    while (tempList.length > 0) {
-      const randomIndex = Math.floor(Math.random() * tempList.length);
-      const randomElement = tempList.splice(randomIndex, 1)[0];
-      randomList.push(randomElement);
+    function startTrivia() {
+        setNumber(0);
+        setUserAnswers([]);
+        setScore(0);
+        setGameOver(false);
+        setRandomQuizData(generateRandomList(getQuizData()));
+        setTimerValue(0);
     }
-  
-    return randomList;
-  }
 
-  function checkAnswer(e: React.MouseEvent<HTMLButtonElement>) {
-    if (!gameOver) {
+    function generateRandomList<T>(elements: T[]): T[] {
+        const tempList = [...elements]; // Copy the initial List
+        const randomList: T[] = [];
 
-      const answer = e.currentTarget.value;
-      const correct = randomQuizData[number].correctAnswer === answer;
+        while (tempList.length > 0) {
+            const randomIndex = Math.floor(Math.random() * tempList.length);
+            const randomElement = tempList.splice(randomIndex, 1)[0];
+            randomList.push(randomElement);
+        }
 
-      if (correct){
-        setScore((prev) => prev + 100);
-      }
-
-      const answerObject = {
-        question: randomQuizData[number].question,
-        answer,
-        correct,
-        correctAnswer: randomQuizData[number].correctAnswer,
-      };
-      setUserAnswers((prev) => [...prev, answerObject]);
+        return randomList;
     }
-  }
 
-  function nextQuestion() {
-    const nextNumber = number + 1;
+    function checkAnswer(e: React.MouseEvent<HTMLButtonElement>) {
+        if (!gameOver) {
 
-    if (nextNumber === MAX_QUESTIONS) {
-      setGameOver(true)
-    } else {
-      setNumber(nextNumber);
+            const answer = e.currentTarget.value;
+            const correct = randomQuizData[number].correctAnswer === answer;
+
+            if (correct) {
+                setScore((prev) => prev + 100);
+            }
+
+            const answerObject = {
+                question: randomQuizData[number].question,
+                answer,
+                correct,
+                correctAnswer: randomQuizData[number].correctAnswer,
+            };
+            setUserAnswers((prev) => [...prev, answerObject]);
+        }
     }
-  }
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container sx={{ textAlign: "center" }}>
-        <Typography variant="h3" component="h2" sx={{mt:6, color: "white"}}>Typescript Quiz</Typography>
-        
-        {gameOver && (
-        <Button variant="contained" onClick={startTrivia} sx={{mt:3}} role="startButton">
-          Start
-        </Button>
-        ) }
+    function nextQuestion() {
+        const nextNumber = number + 1;
 
-        {userAnswers.length === MAX_QUESTIONS && !gameOver && (
-          <>
-            <WinningMessage timerValue={timerValue} score={score} />
-            <Button variant="contained" onClick={startTrivia} sx={{mt:3}} role="startButton">
-            Restart
-          </Button>
-          </>
-        )}
+        if (nextNumber === MAX_QUESTIONS) {
+            setGameOver(true)
+        } else {
+            setNumber(nextNumber);
+        }
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container sx={{textAlign: "center"}}>
+                <Typography variant="h3" component="h2" sx={{mt: 6, color: "white"}}>Typescript Quiz</Typography>
+
+                {gameOver && (
+                    <Button variant="contained" onClick={startTrivia} sx={{mt: 3}} role="startButton">
+                        Start
+                    </Button>
+                )}
+
+                {userAnswers.length === MAX_QUESTIONS && !gameOver && (
+                    <>
+                        <WinningMessage timerValue={timerValue} score={score}/>
+                        <Button variant="contained" onClick={startTrivia} sx={{mt: 3}} role="startButton">
+                            Restart
+                        </Button>
+                    </>
+                )}
 
 
-        {!gameOver && userAnswers.length !== MAX_QUESTIONS && (
-          <>
-            <Typography sx={{color:"white", mt:3}} component={'span'}>
-              Score: {score}
-              <Button variant="contained" onClick={nextQuestion} sx={{ml:5}} role="nextButton">
-                Next
-              </Button>
-              <Timer onTimeChange={setTimerValue} />
-            </Typography>
+                {!gameOver && userAnswers.length !== MAX_QUESTIONS && (
+                    <>
+                        <Typography sx={{color: "white", mt: 3}} component={'span'}>
+                            Score: {score}
+                            <Button variant="contained" onClick={nextQuestion} sx={{ml: 5}} role="nextButton">
+                                Next
+                            </Button>
+                            <Timer onTimeChange={setTimerValue}/>
+                        </Typography>
 
-            <QuestionCard
-              questionNr={number + 1}
-              totalQuestions={MAX_QUESTIONS}
-              question={randomQuizData[number].question}
-              answers={randomQuizData[number].answers}
-              userAnswer={userAnswers ? userAnswers[number] : undefined}
-              callback={checkAnswer} 
-            />
-          </> 
-          )}
-        </Container>
-      </ThemeProvider>
-  )
+                        <QuestionCard
+                            questionNr={number + 1}
+                            totalQuestions={MAX_QUESTIONS}
+                            question={randomQuizData[number].question}
+                            answers={randomQuizData[number].answers}
+                            userAnswer={userAnswers ? userAnswers[number] : undefined}
+                            callback={checkAnswer}
+                        />
+                    </>
+                )}
+            </Container>
+        </ThemeProvider>
+    )
 }
